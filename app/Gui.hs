a59b3c4516110fdec6c740dbadcc66e3af5bd9d4
Tobias Theus <tobiastheus@googlemail.com>
2020-09-12 21:03:00 +0200

    Added missing elements to createSheet method

--- app/Gui.hs
+++ app/Gui.hs
@@ -76,23 +76,38 @@ initialiseGui title = do
     vBox <- Gtk3.vBoxNew True 5
     hBox2 <- Gtk3.hBoxNew True 5
     hBox3 <- Gtk3.hBoxNew True 5
+    hBox4 <- Gtk3.hBoxNew True 5
+    hBox5 <- Gtk3.hBoxNew True 5
     button1 <- Gtk3.buttonNewWithLabel "Select output directory"
     button2 <- Gtk3.buttonNewWithLabel "Create File"
     comboBox <- Gtk3.comboBoxNewText
     comboBox2 <- Gtk3.comboBoxNewText
+    comboKey <- Gtk3.comboBoxNewText
     entChord <- Gtk3.entryNew
     labChord <- Gtk3.labelNew (Just "Chord progression:")
+    labKey <- Gtk3.labelNew (Just "Key:")
+    labOctave <- Gtk3.labelNew (Just "Octave:")
     labFolderHead <- Gtk3.labelNew (Just "Filename:")
+    labBars <- Gtk3.labelNew (Just "Bars:")
+    labNotes <- Gtk3.labelNew (Just "Notes:")
     labFolder <- Gtk3.labelNew (Just filepath)
     checkCustom <- Gtk3.checkButtonNewWithLabel "Custom Mode"
+    scaleOct <- Gtk3.hScaleNewWithRange (-5.0) (5.0) (0.1)
+    spinBars <- Gtk3.spinButtonNewWithRange 1 20 1
+    spinNotes <- Gtk3.spinButtonNewWithRange 1 20 1
 
     -- Setup GUI elements
     Gtk3.on window Gtk3.objectDestroy Gtk3.mainQuit
     Gtk3.set window [ Gtk3.containerBorderWidth Gtk3.:= 10, Gtk3.windowTitle Gtk3.:= title ]
     Gtk3.entrySetText entChord "1 5 6 4"
+    Gtk3.rangeSetValue scaleOct (0.0)
+    Gtk3.spinButtonSetValue spinBars 4
+    Gtk3.spinButtonSetValue spinNotes 4
 
     Gtk3.containerAdd vBox hBox2
     Gtk3.containerAdd vBox hBox
+    Gtk3.containerAdd vBox hBox4
+    Gtk3.containerAdd vBox hBox5
     Gtk3.containerAdd vBox hbuttonbox
     Gtk3.containerAdd vBox hBox3
     Gtk3.containerAdd window vBox
@@ -101,6 +116,14 @@ initialiseGui title = do
     Gtk3.containerAdd hBox entChord
     Gtk3.containerAdd hBox comboBox2
     Gtk3.containerAdd hBox comboBox
+    Gtk3.containerAdd hBox4 labKey
+    Gtk3.containerAdd hBox4 comboKey
+    Gtk3.containerAdd hBox4 labOctave
+    Gtk3.containerAdd hBox4 scaleOct
+    Gtk3.containerAdd hBox5 labBars
+    Gtk3.containerAdd hBox5 spinBars
+    Gtk3.containerAdd hBox5 labNotes
+    Gtk3.containerAdd hBox5 spinNotes
     Gtk3.containerAdd hBox3 labFolderHead
     Gtk3.containerAdd hBox3 labFolder
     
@@ -111,12 +134,15 @@ initialiseGui title = do
 
     sequence $ setupComboBox comboBox ["Twelve Bar Blues", "Axis of Awesome", "Pessimistic", "Pop", "JazzCat", "Pachelbel"]
     sequence $ setupComboBox comboBox2 ["Major", "Minor"]
+    sequence $ setupComboBox comboKey ["A", "As", "B", "C", "Cs", "D", "Ds", "E", "F", "Fs", "G", "Gs"]
+
     Gtk3.comboBoxSetActive comboBox 0
     Gtk3.comboBoxSetActive comboBox2 0
+    Gtk3.comboBoxSetActive comboKey 0
 
     -- Setup event handlers
     Gtk3.on button1 Gtk3.buttonActivated $ openSelectFolderDialog window labFolder
-    Gtk3.on button2 Gtk3.buttonActivated $ onStartButtonClicked comboBox comboBox2 labFolder createSheet checkCustom entChord
+    Gtk3.on button2 Gtk3.buttonActivated $ onStartButtonClicked comboBox comboBox2 labFolder createSheet checkCustom entChord comboKey scaleOct spinBars spinNotes
     Gtk3.on checkCustom Gtk3.toggled (onCustomChecked hBox entChord comboBox checkCustom)
 
     -- Finish GUI setup
@@ -136,8 +162,19 @@ createChordProgression :: String -> [Int]
 createChordProgression input = map (read::String->Int) list
     where list = words input
 
-onStartButtonClicked :: Gtk3.ComboBox -> Gtk3.ComboBox -> Gtk3.Label -> (FilePath -> [Progression] -> Key -> Octave -> MajMin-> Int -> NumNotes -> IO ()) -> Gtk3.CheckButton -> Gtk3.Entry -> IO()
-onStartButtonClicked comboBox comboBoxMiMa fileLabel createSheet customTB customEnt = do
+onStartButtonClicked :: Gtk3.ComboBox -> Gtk3.ComboBox -> Gtk3.Label -> (FilePath -> [Progression] -> Key -> Octave -> MajMin-> Int -> NumNotes -> IO ()) -> Gtk3.CheckButton -> Gtk3.Entry -> Gtk3.ComboBox -> Gtk3.HScale -> Gtk3.SpinButton -> Gtk3.SpinButton -> IO()
+onStartButtonClicked comboBox comboBoxMiMa fileLabel createSheet customTB customEnt comboKeys scaleOctave spinBars spinNotes = do
+    --spinBars spinNotes
+    keysText <- Gtk3.comboBoxGetActiveText comboKeys
+    let selKey = convertText((fromMaybe (toText "A") keysText) :: T.Text) :: String
+    let key = selectKey selKey
+
+    octaveRangeValue <- Gtk3.rangeGetValue scaleOctave
+    let octaveValue = (realToFrac octaveRangeValue) :: Float
+
+    barsValue <- Gtk3.spinButtonGetValueAsInt spinBars
+    notesValue <- Gtk3.spinButtonGetValueAsInt spinNotes
+    
     status <- Gtk3.toggleButtonGetActive customTB
     entText <- (Gtk3.entryGetText customEnt) :: IO([Char])
 
@@ -154,24 +191,39 @@ onStartButtonClicked comboBox comboBoxMiMa fileLabel createSheet customTB custom
     if status
         then do
             let chordProg = createChordProgression entText
-            async $ createSheet filepath chordProg A octave miMa 4 4
+            async $ createSheet filepath chordProg key octaveValue miMa barsValue notesValue
             return ()
         else do
             case choice of 
                 "Twelve Bar Blues" -> do
-                    async $ createSheet filepath twelveBarBlues A octave miMa 4 4
+                    async $ createSheet filepath twelveBarBlues key octaveValue miMa barsValue notesValue
                 "Axis of Awesome"-> do
-                    async $ createSheet filepath axisOfAwesome A octave miMa 4 4
+                    async $ createSheet filepath axisOfAwesome key octaveValue miMa barsValue notesValue
                 "Pessimistic" -> do 
-                    async $ createSheet filepath pessimistic A octave miMa 4 4
+                    async $ createSheet filepath pessimistic key octaveValue miMa barsValue notesValue
                 "Pop"-> do 
-                    async $ createSheet filepath pop1 A octave miMa 4 4
+                    async $ createSheet filepath pop1 key octaveValue miMa barsValue notesValue
                 "JazzCat" -> do 
-                    async $ createSheet filepath jazzCat A octave miMa 4 4
+                    async $ createSheet filepath jazzCat key octaveValue miMa barsValue notesValue
                 "Pachelbel" -> do 
-                    async $ createSheet filepath pachelbel A octave miMa 4 4
+                    async $ createSheet filepath pachelbel key octaveValue miMa barsValue notesValue
             return ()
 
+-- data Key = A | As | B | C | Cs | D | Ds | E | F | Fs | G | Gs deriving (Enum, Show, Eq, Bounded)
+selectKey :: String -> Key
+selectKey text 
+    | text == "A" = A
+    | text == "As" = As
+    | text == "B" = B
+    | text == "C" = C
+    | text == "Cs" = Cs
+    | text == "D" = D
+    | text == "Ds" = Ds
+    | text == "E" = E
+    | text == "F" = F
+    | text == "Fs" = Fs
+    | text == "G" = G
+    | otherwise = Gs
 
 -- Remove " and ' from text String.
 removeParantheses :: String -> String
